af62d6a5d664bccd367886b438fcdee4bf17981a
natalia-glejzer <natalia.glejzer@interia.pl>
2025-04-12 14:12:20 +0200

    Speach to text code

---
+++ speach_to_text/stp.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+import pyttsx3
+import os
+
+r=sr.Recognizer()
+
+print(os.getcwd())
+
+def record_text():
+    try:
+        with sr.Microphone() as source2:
+            r.adjust_for_ambient_noise(source2, duration=3)
+            audio2 = r.listen(source2)
+            MyText = r.recognize_google(audio2, language = 'pl')
+            return MyText 
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("Unknown error")
+
+    return ""
+    
+
+def output_text(text):
+    f = open('output.txt', 'a', encoding='utf-8')
+    f.write(text)
+    f.write('\n')
+    f.close()
+    return
+
+while(1):
+    text = record_text()
+    output_text(text)
+
+    print("Output saved!")
