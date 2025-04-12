f1054d9c958e0ea69819783fd8ba22f13b8ed7a1
natalia-glejzer <natalia.glejzer@interia.pl>
2025-04-12 16:08:24 +0200

    json format

--- google_map/google_api.py
+++ google_map/google_api.py
@@ -8,4 +8,5 @@ address = "parafia królowej jadwigi rzeszów"
 url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={api_key})"
 
 response = requests.get(url).json()
-print(json.dumps(response))
+data = json.loads(response)
+df = pd.json_normalize(data['results'])
